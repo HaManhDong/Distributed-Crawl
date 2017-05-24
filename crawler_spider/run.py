7222c6fcee379cefd825fca071d360420d0ce6f6
huynhducbk95 <huynhducpro95@gmail.com>
2017-05-24 23:56:05 +0700

    create project crawler_spider

---
+++ crawler_spider/run.py
@@ -0,0 +1,15 @@
+import os
+import threading
+from scrapy.crawler import Crawler
+import time
+
+def foo():
+    # time.sleep(4)
+    command = "scrapy crawl abcnews -a url='http://abcnews.go.com/Politics/trump-asked-nsa-director-publicly-push-back-fbis/story?id=47578374&cid=clicksource_77_null_headlines_hed'"
+    os.system(command)
+
+
+t = threading.Thread(target=foo,name='test')
+t.start()
+print 'xxxxxx'
+threading.currentThread().__setattr__('next_urls',None)
